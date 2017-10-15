1c30b4e240414868df5c9d00d68afd75157da822
Sdzeng <zsongdong@163.com>
2017-10-15 17:03:29 +0800

    新增

---
+++ Kard.Core/Entities/CommentEntity.cs
@@ -0,0 +1,21 @@
+﻿using Kard.Domain.Entities.Auditing;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kard.Core.Entities
+{
+    public class CommentEntity: DeletionAuditedEntity
+    {
+        public long Id { get; set; }
+
+        public long ParentId { get; set; }
+
+        public long EssayId { get; set; }
+
+        public string Content { get; set; }
+
+        public long LikeNum { get; set; }
+
+    }
+}
---
+++ Kard.Core/Entities/RepostEntitycs.cs
@@ -0,0 +1,16 @@
+﻿using Kard.Domain.Entities.Auditing;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kard.Core.Entities
+{
+    public class RepostEntity: DeletionAuditedEntity
+    {
+
+        public long Id { get; set; }
+
+        public long EssayId { get; set; }
+
+    }
+}
