59fb5ec03c40fb3988012702643f44c0658b913f
Sdzeng <zsongdong@163.com>
2018-03-24 19:12:51 +0800

    add options

--- Kard.Client/wxkard/pages/task/add.js
+++ Kard.Client/wxkard/pages/task/add.js
@@ -1,73 +1,56 @@
-// Page({
-//   onLoad: function (options) {
-//     var duanziId = options.operation;
-//     this.setData({ duanziId});
-//     var that = this;
-//     wx.showModal({
-//       title: '提示',
-//       content: '这是一个模态弹窗',
-//       success: function (res) {
-//         if (res.confirm) {
-//           console.log('用户点击确定')
-
-//           wx.showActionSheet({
-//             itemList: ['A', 'B', 'C'],
-//             success: function (res) {
-//               console.log(res.tapIndex)
-//             },
-//             fail: function (res) {
-//               console.log(res.errMsg)
-//             }
-//           })
-
-
-//         } else if (res.cancel) {
-//           console.log('用户点击取消')
-//         }
-//       }
-//     })
- 
-//   } 
-
-// })
-
+const app = getApp();
 
 Page({
-  data:{
-    isLongTerm:false,
-    weekList:['日', '一', '二', '三', '四', '五', '六']
+  data: {
+    isLongTerm: false,
+    weekList: ['日', '一', '二', '三', '四', '五', '六']
 
   },
-  switchWeekList:function(e){
+  switchIsLongTerm: function (e) {
     this.data.isLongTerm = e.detail.value;
     this.setData({
       isLongTerm: this.data.isLongTerm
     });
   },
-  chooseTaskStartDate:function(e){
-  //   <picker-view class="calender_info_choose_yearmonths_picker_view" indicator- style="height: 50px;" value= "{{pickerValue}}" bindchange= "pickerChange" >
-  //     <picker-view - column >
-  //     <view class="calender_info_choose_yearmonths_picker_view_cols" wx: for="{{pickerYearList}}" wx:key = "*this" > {{item }
-  // }年</view >
-  // </picker-view-column>
-  // < picker - view - column >
-  // <view class="calender_info_choose_yearmonths_picker_view_cols" wx:for="{{pickerMonthList}}" wx:key = "*this" > {{item }}月 < /view>
-  //   < /picker-view-column>
-  //   < /picker-view>
-
-    wx.showModal({
-      title: '开始日期',
-      content: '&lt;view&gt;ggggg&lt;/view&gt;',
-      success: function (res) {
-        if (res.confirm) {
-          console.log('用户点击确定')
-        } else if (res.cancel) {
-          console.log('用户点击取消')
-        }
-      }
-    })
+  chooseTaskStartDate: function (e) {
+    let pickerYearList = [];
+    let pickerMonthList = [];
+    for (let i = 1900; i <= 2200; i++) {
+      pickerYearList.push(i);
+    }
+    for (let i = 1; i <= 12; i++) {
+      pickerMonthList.push(i);
+    }
+    const idxYear = pickerYearList.indexOf(app.globalData.currentDate.year);
+    const idxMonth = pickerMonthList.indexOf(app.globalData.currentDate.month);
+    this.setData({
+      pickerValue: [idxYear, idxMonth],
+      pickerYearList,
+      pickerMonthList,
+      showPicker: true,
+      newYearMonthGrids: null
+    });
 
 
+
+  },
+  preventTouchMove() { },
+  pickerChange(e) {
+    const val = e.detail.value;
+    let newYearMonthGrids = { year: this.data.pickerYearList[val[0]], month: this.data.pickerMonthList[val[1]] };
+    this.setData({
+      newYearMonthGrids
+    });
+  },
+  tapPickerBtn(e) {
+    const type = e.currentTarget.dataset.type;
+    if (type === 'confirm' && this.data.newYearMonthGrids != null) {
+      this.calculateDays(this.data.newYearMonthGrids.year, this.data.newYearMonthGrids.month);
+    }
+
+    this.setData({
+      showPicker: false,
+    });
   },
   formSubmit: function (e) {
     console.log('form发生了submit事件，携带数据为：', e.detail.value)
@@ -76,5 +59,5 @@ Page({
     wx.navigateBack({
       delta: 1
     })
-  } 
+  }
 })
