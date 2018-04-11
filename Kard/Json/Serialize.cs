36711982c759229d03a992f0ce9dd6c8013b49e9
Sdzeng <zsongdong@163.com>
2018-04-11 16:12:47 +0800

    添加

---
+++ Kard/Json/Serialize.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+
+namespace Kard.Json
+{
+    public class Serialize
+    {
+        public static T FromJson<T>(string str)
+        {
+            return JsonConvert.DeserializeObject<T>(str);
+        }
+        public static string ToJson<T>(T value)
+        {
+            var jSetting = new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore };
+            return JsonConvert.SerializeObject(value, Formatting.None, jSetting);
+        }
+    }
+}
