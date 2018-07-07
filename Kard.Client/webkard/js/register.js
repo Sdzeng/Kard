9d521f70e3e6b47599e7237da72de5250547e714
Sdzeng <zsongdong@163.com>
2018-07-07 11:40:02 +0800

    v1.3

---
+++ Kard.Client/webkard/js/register.js
@@ -0,0 +1,62 @@
+var registerjs = {
+    data: { scope: $("#registerPage") },
+    init: function () {
+        var _this = this;
+        _this.bindCover();
+        _this.login();
+    },
+    bindCover: function () {
+        var _this = this;
+
+        //设置首页封面
+        topCover.getHomeCover(function (resultDto) {
+            var data = resultDto.data;
+            //data = JSON.parse(data);
+            if (!data) {
+                return;
+            }
+            $(".bg-layer", _this.data.scope).css("background-image", "linear-gradient(to bottom, rgba(0, 0, 0, 0.3) 0%, rgba(0, 0, 0, 0.3) 100%),url(" + basejs.cdnDomain + "/" + (data.media.cdnPath + "_2560x1200." + data.media.mediaExtension || "") + ")");
+        });
+    },
+    login: function () {
+        var _this = this;
+ 
+        $(".register-form", _this.data.scope).submit(function (event) {
+
+            // 阻止表单提交  
+            event.preventDefault();
+
+            var helper = new httpHelper({
+                url: basejs.requestDomain + "/webuser/register",//this.url || this.form.action,
+                type: 'POST',
+                //contentType: "application/json;charset=utf-8",
+                data: $(this).serialize(),//{"username":$("#username").val()},//
+                success: function (data) {
+                    //var result = JSON.parse(data);
+                    if (data.result) {
+                   
+                        window.location.href = "/login.html";
+                    }
+                    else {
+                    
+                        alert(data.message);
+
+                    }
+                },
+                error: function () {
+                
+                }
+            });
+
+            helper.send();
+        });
+
+    }
+};
+
+$(function () {
+    //菜单
+    topMenu.bindMenu();
+
+    registerjs.init();
+});
---
+++ Kard.Core/Dtos/EssayCommentDto.cs
@@ -0,0 +1,26 @@
+﻿using Kard.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kard.Core.Dtos
+{
+    public class EssayCommentDto
+    {
+        public long Id { get; set; }
+
+        public long? ParentId { get; set; }
+
+        public long EssayId { get; set; }
+
+        public string Content { get; set; }
+
+        public long LikeNum { get; set; }
+
+        public DateTime CreationTime { get; set; }
+
+        public KuserEntity Kuser;
+
+        public IEnumerable<EssayCommentDto> ParentCommentDtoList { get; set; }
+    }
+}
